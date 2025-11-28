2487a6c7bca20db1d70a75e010c9b196cb73a791
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-11-28 13:45:32 +0800

    add route for register user

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthenticationControlller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthenticationControlller::class, 'register']);
